5111eeecc93c8d3890337841f8fca74dd2fd4dab
basavkiran <66815786+basavkiran@users.noreply.github.com>
2024-01-15 12:54:47 +0530

    Add files via upload

---
+++ train app/Untitled-1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str = "javapoint";
+    string substring = " ";
+    substring = str.substr(0,3);
+    cout<<"Substring is: "<<substring;
+
+}
